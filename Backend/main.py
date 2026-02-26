95c41b8c447433ed3e3bc004d18041573fc299af
ParthTiwari-123 <149923567+ParthTiwari-123@users.noreply.github.com>
2026-02-26 13:03:19 +0000

    Backend Fix

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+import shutil
+
 from transformers import pipeline
 from PIL import Image
-from facenet_pytorch import MTCNN
-import cv2
-import numpy as np
 
 app = FastAPI()
 
-# CORS enable
+classifier = pipeline(
+    "image-classification",
+    model="dima806/deepfake_vs_real_image_detection"
+)
+
+# CORS enable (React ko allow karega)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,49 +21,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Face detector
-mtcnn = MTCNN(keep_all=False)
-
-# Deepfake detection model (ViT based)
-classifier = pipeline(
-    "image-classification",
-    model="prithivMLmods/DeepFake-Detection-Model"
-)
-
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
 
-    # Convert uploaded image to PIL
     image = Image.open(file.file).convert("RGB")
 
-    # Detect face
-    boxes, _ = mtcnn.detect(image)
+    result = classifier(image)
 
-    if boxes is None:
-        return {
-            "prediction": "No Face Detected",
-            "confidence": 0.0
-        }
-
-    # Crop first face
-    x1, y1, x2, y2 = map(int, boxes[0])
-    face = image.crop((x1, y1, x2, y2))
-
-    # Run deepfake model on face
-    result = classifier(face)
-
+    label = result[0]['label']
     score = result[0]['score']
 
-    # Threshold logic
-    if score > 0.75:
-        prediction = "Fake"
-    elif score < 0.35:
-        prediction = "Real"
-    else:
-        prediction = "Uncertain"
-
     return {
-        "prediction": prediction,
+        "prediction": label,
         "confidence": float(score)
     }
+
+
 # uvicorn main:app --reload
